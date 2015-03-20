10b72eb392cf7d0ded1a6c2bacaac0b154dd598d
william <chihtong.lin@gmail.com>
2015-03-20 11:33:12 -0400

    result class

---
+++ src/Result.java
@@ -0,0 +1,18 @@
+
+public class Result{
+	int league;
+	float distance;
+	
+	public Result(int league, float distance){
+		this.league = league;
+		this.distance = distance;
+	}
+	
+	public int getLeague(){
+		return league;
+	}
+	
+	public float getDistance(){
+		return distance;
+	}
+}
