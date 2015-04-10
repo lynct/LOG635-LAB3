fcbbc8a3556cb8e56a5b190bb66e239b530cffc3
ak36220 <ak36220@logti-a3322-06.ens.ad.etsmtl.ca>
2015-04-10 10:20:22 -0400

    Modification au Main pour faciliter le debug de l'évaluation.

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,22 @@ public class Main {
 	public static void main(String[] args) {
 		
 		/*****************************************************
-		 * CHANGER isTraining A FALSE POUR EVALUATION.
+		 * CHANGER isDebug A FALSE POUR EVALUATION.
 		 *****************************************************/
-		boolean isTraining = true;
+		boolean isDebug = false;
 		int total=0, nbCorrect=0, nbIncorrect=0;	//statistics variables
 		//Reading players information from file
-		ArrayList<Player> dataTraining = DataReader.run("Donnees_sources.csv", true, false, false);
-		ArrayList<Player> dataEvaluation = DataReader.run("Donnees_sources.csv", true, true, false);
-		/*
-		ArrayList<Player> dataTraining = DataReader.run("Donnees_sources.csv", true, false, true);
-		ArrayList<Player> dataEvaluation = DataReader.run("evaluation.csv", false, true, true);
-		*/
+		ArrayList<Player> dataTraining;
+		ArrayList<Player> dataEvaluation;
+		if(isDebug){
+			dataTraining = DataReader.run("Donnees_sources.csv", true, false, false);
+			dataEvaluation = DataReader.run("Donnees_sources.csv", true, true, false);
+		}
+		else{
+			dataTraining = DataReader.run("Donnees_sources.csv", true, false, true);
+			dataEvaluation = DataReader.run("evaluation.csv", false, true, true);
+		}
+		
 		ArrayList<Result> resultList = new ArrayList<Result>();
 		
 		//Scaling the players values
@@ -31,7 +36,8 @@ public class Main {
 			resultList = KNearestNeighbourAlgorithm.getNearestNeighbors(resultList);
 			int league = KNearestNeighbourAlgorithm.findMajority(resultList);
 			
-			if(isTraining){
+			//
+			if(isDebug){
 				total++;
 				//On est correct a + ou - 1 de la vraie valeur de la league.
 				if(p.getLeague() == league || p.getLeague() == league+1 || p.getLeague() == league-1){
@@ -46,7 +52,7 @@ public class Main {
 			}
 		}
 		
-		if(isTraining){
+		if(isDebug){
 			System.out.println("Total: " + String.valueOf(total));
 			System.out.println("Correct: " + String.valueOf(nbCorrect));
 			System.out.println("Incorrect: " + String.valueOf(nbIncorrect));
